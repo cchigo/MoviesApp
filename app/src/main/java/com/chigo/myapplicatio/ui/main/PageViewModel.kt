e75ad9650ccbaff8fd62180a22891e2c8f86093d
chigo <ccasadu15@gmail.com>
2023-07-09 08:03:40 +0100

    Initial commit

---
+++ app/src/main/java/com/chigo/myapplicatio/ui/main/PageViewModel.kt
@@ -0,0 +1,14 @@
+package com.chigo.myapplicatio.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PageViewModel : ViewModel() {
+
+    private val _index = MutableLiveData<Int>()
+
+
+    fun setIndex(index: Int) {
+        _index.value = index
+    }
+}
